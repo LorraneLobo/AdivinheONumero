03e1b065ff67d0743254dd36c8d9fe8b4519f447
Lorrane Alves <lorrane.dev@gmail.com>
2018-08-13 22:42:37 -0300

    Computador gera um numero entre 1 e 1000

--- src/jogos/AdivinheONumero.java
+++ src/jogos/AdivinheONumero.java
@@ -1,5 +1,16 @@
 package jogos;
 
+import java.util.Random;
+
 public class AdivinheONumero {
 
+	
+	public static void main(String[] args) {
+		
+		Random geradorAleatorio = new Random();
+		
+		//Computador vai gerar um numero aleatório entre 0 e 999, adicionamos 1 para ser entre 1 e 1000
+		int sorteado = geradorAleatorio.nextInt(1000)+1;
+		
+	}
 }
